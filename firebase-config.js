fc87d0789786756d6958439cfe3583e9dcd8ccdb
nicaisedegbey1-lab <nicaisedegbey1@gmail.com>
2026-09-09 21:18:12 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+/*
+  Configuration Firebase du projet "nid-gourmand".
+*/
+const firebaseConfig = {
+  apiKey: "AIzaSyDxd7YGu8qJNCmbR9g4Ny1fPnaGmW6n2LU",
+  authDomain: "nid-gourmand.firebaseapp.com",
+  projectId: "nid-gourmand",
+  storageBucket: "nid-gourmand.firebasestorage.app",
+  messagingSenderId: "978834940062",
+  appId: "1:978834940062:web:3665e8065f1a6d14e31b8f"
+};
